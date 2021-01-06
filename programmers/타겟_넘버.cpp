2a47e12becf6e1269d62d101728696f31557374e
홍지영 <61260814+Jiyoung-h@users.noreply.github.com>
2021-01-06 21:39:37 +0900

    Create 타겟_넘버.cpp

---
+++ programmers/타겟_넘버.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    for(int i=1; i<(1<<numbers.size()); i++){
+        int sum = 0;
+        for(int j=0; j<numbers.size(); j++){
+            if(i&(1<<j)){
+                sum += numbers[j];
+            }
+            else{
+                sum -= numbers[j];
+            }
+        }
+        if(sum == target) answer++;
+    }
+    return answer;
+}
