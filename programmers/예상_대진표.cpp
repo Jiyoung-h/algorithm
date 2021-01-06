c82ee1d734819af05aaa02f34d0ba4c9600af92f
홍지영 <61260814+Jiyoung-h@users.noreply.github.com>
2021-01-06 23:51:15 +0900

    Update 예상_대진표.cpp

--- programmers/예상_대진표.cpp
+++ programmers/예상_대진표.cpp
@@ -2,26 +2,13 @@ using namespace std;
 
 int solution(int n, int a, int b)
 {
-    int temp = 1;
-    int round = 0;
-    for(int i=1; i<=20; i++){
-        temp *= 2;
-        round++;
-        if(temp == n) break;
+    int answer = 0;
+    while(a!=b){
+       if(a%2!=0) a=a+1;
+       if(b%2!=0) b=b+1;
+       a=a/2;
+       b=b/2;
+       answer++;
     }
-    int front = 1;
-    int back = n;
-    while(1){
-        if(a>((back+front)/2) && b>((back+front)/2)){
-            front = back/2 + 1;
-            round--;
-        }
-        else if(a<=((back+front)/2) && b<=((back+front)/2)){
-            back = back/2;
-            round--;
-        }
-        else{
-            return round;
-        }
-    }
+    return answer;
 }
