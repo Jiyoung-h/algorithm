942b0abde27804d85c192662bf033a8283036805
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-01-25 14:05:12 +0900

    가장 큰 정사각형 찾기.cpp

--- programmers/가장 큰 정사각형 찾기.cpp
+++ programmers/가장 큰 정사각형 찾기.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int solution(vector<vector<int>> board)
-{
-   int answer = 0;
 
-	for (int i = 0; i < board.size(); i++) {
-		for (int j = 0; j < board[0].size(); j++) {
-			if (board[i][j] == 1) {
-				int n = 1;
-				while(1) {
-					if (i+n>board.size()-1||j+n>board[0].size()-1|| board[i + n][j + n] != 1) {
-						answer = max(n, answer);
-						break;
-					}
-					int p, q;
-					for (p = 0; p < n; p++) {
-						if (board[i + p][j + n] != 1) { answer = max(n, answer); break; }
-					}
-					for (q = 0; q < n; q++) {
-						if (board[i + n][j + q] != 1) { answer = max(n, answer); break; }
-					}
-					if (p != n || q != n) break;
-					n++;
-				}
-			}
-		}
-	}
-	return answer*answer;
-}
+int solution(vector<vector<int>> board){
+    int answer = board[0][0]; 
+    
+    for(int i = 1; i < board.size(); i++){
+        for(int j = 1; j <  board[0].size(); j++){
+            if(board[i][j] == 1){ 
+                board[i][j] = min(board[i][j - 1], min(board[i - 1][j], board[i - 1][j - 1]))+1;
+                answer = max(answer, board[i][j]);
+            }
+        }
+    }
+
+  
