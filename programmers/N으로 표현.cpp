2e79f2d48abd7f97bf29c09d135006479d9d0ec5
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-02-01 13:25:14 +0900

    Update N으로 표현.cpp

--- programmers/N으로 표현.cpp
+++ programmers/N으로 표현.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 int solution(int N, int number) {
 	int answer = -1, std = 0;
-	unordered_set<int> us[MAXN];
+	// 원소의 개수가 적고 빠른 성능을 원할 땐 unordered_set
+	unordered_set<int> us[MAXN];	// us[횟수] 
 	for (int i = 0; i < MAXN; i++) {
-		std = 10 * std + 1;
-		us[i].insert(std * N);
+		std = 10 * std + 1;	// 1, 11, 111, ...
+		us[i].insert(std * N);	// 5, 55, 555, ...
 	}
-	for (int i = 1; i < MAXN; i++) {
+	for (int i = 1; i < MAXN; i++) { // i : 연산 횟수
 		for (int j = 0; j < i; j++) {
-			for (auto& op1 : us[j]) {
-				for (auto& op2 : us[i - j - 1]) {
-					us[i].insert(op1 + op2);
-					us[i].insert(op1 - op2);
+			for (auto& op1 : us[j]) {			// op1 : j번 연산한 값
+				for (auto& op2 : us[i - j - 1]) {	// op2 : (i-j-1)번 연산한 값
+					us[i].insert(op1 + op2);	// op1 과 op2 를 연산해야 연산횟수가 i 번
+ 					us[i].insert(op1 - op2);
 					us[i].insert(op1 * op2);
 					if (op2 != 0) {
 						us[i].insert(op1 / op2);
@@ -22,7 +23,7 @@ int solution(int N, int number) {
 				}
 			}
 		}
-		if (us[i].count(number) > 0) {
+		if (us[i].count(number) > 0) {	// number가 존재하면 멈춤
 			answer = i + 1;
 			break;
 		}
