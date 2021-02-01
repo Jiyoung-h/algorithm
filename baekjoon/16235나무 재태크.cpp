8f7502a12742b31a2f3e7dbdaa40b7652f86c1a3
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-02-01 14:54:29 +0900

    Update 16235나무 재태크.cpp

--- baekjoon/16235나무 재태크.cpp
+++ baekjoon/16235나무 재태크.cpp
@@ -15,8 +15,8 @@ int main() {
     cin >> n >> m >> l;
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            cin >> a[i][j];
-            d[i][j] = 5; // 가장 처음에 양분은 모든 칸에 5만큼 들어있다.
+            cin >> a[i][j]; // 추가되는 양분
+            d[i][j] = 5;    // 양분
         }
     }
     while (m--) {
@@ -28,29 +28,29 @@ int main() {
         memset(p,0,sizeof(p));
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                vector<int> temp;   // 나무의 새로운 나이 배열
-                sort(tree[i][j].begin(), tree[i][j].end()); // 하나의 칸에 여러 개의 나무가 있다면, 나이가 어린 나무부터 양분을 먹는다.
-                int dead = 0;
+                vector<int> temp;   // 나무의 새로운 나이
+                sort(tree[i][j].begin(), tree[i][j].end()); // 나이가 어린 나무부터 양분을 먹음
+                int dead = 0;   // 죽은 나무 양분
                 for (int x : tree[i][j]) {
-                    if (x <= d[i][j]) {
+                    if (x <= d[i][j]) { // 양분이 충분하면
                         d[i][j] -= x; // 나무가 자신의 나이만큼 양분을 먹고, 나이가 1 증가한다.
                         temp.push_back(x+1);
-                        if ((x+1) % 5 == 0) { // 번식하는 나무는 나이가 5의 배수이어야 하며,
+                        if ((x+1) % 5 == 0) { // 번식하는 나무는 나이가 5의 배수
                             for (int k=0; k<8; k++) {
                                 int nx = i+dx[k];
                                 int ny = j+dy[k];
-                                if (0 <= nx && nx < n && 0 <= ny && ny < n) { // 상도의 땅을 벗어나는 칸에는 나무가 생기지 않는다.
-                                    p[nx][ny] += 1; // 인접한 8개의 칸에 나이가 1인 나무가 생긴다.
+                                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                                    p[nx][ny] += 1; // 인접한 8개의 칸에 나이가 1인 나무
                                 }
                             }
                         }
-                    } else { // 만약, 땅에 양분이 부족해 자신의 나이만큼 양분을 먹을 수 없는 나무는 양분을 먹지 못하고 즉시 죽는다.
+                    } else {
                         dead += x/2; // 각각의 죽은 나무마다 나이를 2로 나눈 값이 나무가 있던 칸에 양분으로 추가된다.
                     }
                 }
-                tree[i][j] = temp;
-                d[i][j] += dead;
-                d[i][j] += a[i][j]; // 각 칸에 추가되는 양분의 양은 A[r][c]이고, 입력으로 주어진다.
+                tree[i][j] = temp;  // 새로운 나이
+                d[i][j] += dead;    // 죽은 나무는 양분
+                d[i][j] += a[i][j]; // 양분 추가
             }
         }
         for (int i=0; i<n; i++) {
