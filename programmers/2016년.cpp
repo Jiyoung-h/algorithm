610d3fb7009d7c6eec974d5d61c87a2e0554282f
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-02 00:56:21 +0900

    Create 2016년.cpp

---
+++ programmers/2016년.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string day[7] = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    int date[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int tmp = b;
+    for(int i=0; i<a-1; i++){
+        tmp += date[i];
+    }
+    return day[tmp%7];
+}
