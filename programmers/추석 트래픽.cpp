b25f4f17f31e82ede952d4b3ab623e5917d9f587
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-11 22:55:32 +0900

    Update 추석 트래픽.cpp

--- programmers/추석 트래픽.cpp
+++ programmers/추석 트래픽.cpp
@@ -14,28 +14,34 @@ int solution(vector<string> lines) {
         int ih, im, is, process;
         
         lines[i].pop_back();
-        h = lines[i].substr(11, 2); // 시
-        m = lines[i].substr(14, 2); // 분
-        s = lines[i].substr(17, 2); // 초
-        ms = lines[i].substr(20, 3); // 밀리초
+        h = lines[i].substr(11, 2);
+        m = lines[i].substr(14, 2);
+        s = lines[i].substr(17, 2);
+        ms = lines[i].substr(20, 3);
         process = stof(lines[i].substr(24, 5)) * 1000;
+        // 소수점 숫자를 float로 변환해서 1000을 곱해 정수로 만든다
         
         ih = stoi(h) * 3600 * 1000;
         im = stoi(m) * 60 * 1000;
-        is = stoi(s) * 1000 + stoi(ms); // 모두 밀리초로 변환
+        is = stoi(s) * 1000 + stoi(ms);
+        // 시, 분, 초를 모두 밀리초로 변환
         
-        start_t.push_back(ih + im + is - process + 1);  // 시작시간
-        end_t.push_back(ih + im + is);  //끝시간
+        start_t.push_back(ih + im + is - process + 1);
+        // 시작시간 = 처리시간 - 소요시간
+        end_t.push_back(ih + im + is);
     }
     
     for(int i = 0; i < lines.size(); i++)
     {
-        int end_time = end_t[i] + 1000; // 1초 구간
+        int end_time = end_t[i] + 1000;
+        // 해당 요청이 끝나고부터
+        // 초당 최대 처리량을 계산하기 위해 제한시간 1000 밀리초를 더해준다
         int count = 0;
         
         for(int j = i; j < lines.size(); j++)
         {
-            if(start_t[j] < end_time)   // 시작 시간이 1초가 끝나는 시간보다 앞이면
+            // 제한시간 안에 들어가면 카운트를 더해준다
+            if(start_t[j] < end_time)
                 count++;
         }
         
