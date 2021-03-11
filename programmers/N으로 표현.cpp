087a6c377adb08f60c181dbc4f6a309848f1beb5
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-11 22:57:13 +0900

    Update N으로 표현.cpp

--- programmers/N으로 표현.cpp
+++ programmers/N으로 표현.cpp
@@ -1,28 +1,30 @@
 #include <unordered_set>
+#define MAXN 8
 using namespace std;
 
 int solution(int N, int number) {
 	int answer = -1, std = 0;
-	// 원소의 개수가 적고 빠른 성능을 원할 땐 unordered_set
-	unordered_set<int> s[8];	// us[횟수] 
+	unordered_set<int> us[8];
+    // us[ 연산횟수 - 1 ] = { 나올 수 있는 수 }
 	for (int i = 0; i < 8; i++) {
-		std = 10 * std + 1;	// 1, 11, 111, ...
-		s[i].insert(std * N);	// 5, 55, 555, ...
+		std = 10 * std + 1;
+		us[i].insert(std * N);
 	}
-	for (int i = 1; i < 8; i++) { // i : 연산 횟수
-		for (int j = 0; j < i; j++) {
-			for (auto& op1 : s[j]) {			// op1 : j번 연산한 값
-				for (auto& op2 : s[i - j - 1]) {	// op2 : (i-j-1)번 연산한 값
-					s[i].insert(op1 + op2);	// op1 과 op2 를 연산해야 연산횟수가 i 번
- 					s[i].insert(op1 - op2);
-					s[i].insert(op1 * op2);
+    // EX) us[0] : N, us[7] : NNNNNNNN
+	for (int i = 0; i < 8; i++) {       // 총 연산 횟수 i
+		for (int j = 0; j < i; j++) {   // (j번 연산) □ (i-j-1번 연산) = (i번 연산)
+			for (auto& op1 : us[j]) {
+				for (auto& op2 : us[i - j - 1]) {
+					us[i].insert(op1 + op2);
+					us[i].insert(op1 - op2);
+					us[i].insert(op1 * op2);
 					if (op2 != 0) {
-						s[i].insert(op1 / op2);
+						us[i].insert(op1 / op2);
 					}
 				}
 			}
 		}
-		if (s[i].count(number) > 0) {	// number가 존재하면 멈춤
+		if (us[i].count(number) > 0) { // number가 존재하면 연산 횟수를 반환
 			answer = i + 1;
 			break;
 		}
