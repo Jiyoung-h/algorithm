e8c95c0ae13142736810b0ef5b6614ca329a34e9
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-15 22:06:40 +0900

    Update 네트워크.cpp

--- programmers/네트워크.cpp
+++ programmers/네트워크.cpp
@@ -20,14 +20,17 @@ int solution(int n, vector<vector<int>> computers) {
                 check[j] = true;
             }
         }
+        // 연결되어 있는 컴퓨터를 모두 체크했으면
         if (q.empty()) {
             answer++;
             int k;
+            // 모든 컴퓨터를 체크했으면
             if (check.size() == n) {
                 return answer;
             }
             for (k = 0; k < n; k++) {
                 if (!check[k]) {
+                    // 체크되어있지 않은 컴퓨터가 있으면 큐에 넣고 다시 bfs
                     q.push(k);
                     check[k] = true;
                     break;
