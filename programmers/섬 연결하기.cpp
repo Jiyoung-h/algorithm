a4c8a1756c07b62a11561c63af179622181150af
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-16 13:56:05 +0900

    Update 섬 연결하기.cpp

--- programmers/섬 연결하기.cpp
+++ programmers/섬 연결하기.cpp
@@ -1,58 +1,61 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+ 
 using namespace std;
-bool check(vector<bool> visit);
-
-int solution(int n, vector<vector<int>> costs) {
-	int answer = 0;
-	vector<bool> visit(n);
-	vector<vector<int>> graphs(n,vector<int>(n));
-    
-	int minCost = costs[0][2];
-	int minNode1 = costs[0][0];
-   	int minNode2 = costs[0][1];
-    
-	for (int i = 0; i < costs.size(); i++) {
-		graphs[costs[i][0]][costs[i][1]] = costs[i][2];
-		graphs[costs[i][1]][costs[i][0]] = costs[i][2];
-		if (costs[i][2] < minCost) {
-			minCost = costs[i][2];
-			minNode1 = costs[i][0];
-			minNode2 = costs[i][1];
-		}
-	}
-    
-	answer += minCost;
-	visit[minNode1] = true;
-	visit[minNode2] = true;
-	minCost = -1;
-	
-	while (check(visit)) {
-		for (int i = 0; i < visit.size(); i++) {
-			if (visit[i] != true)
-				continue;
-			for (int j = 0; j < visit.size(); j++) {
-				// 최소비용이 아직 없거나 비용이 최소이고, 방문하지도 않았고, 
-				if ((minCost == -1 || minCost > graphs[i][j]) && visit[j] != true && graphs[i][j] != 0) {
-					minCost = graphs[i][j];
-					minNode1 = i;
-					minNode2 = j;
-				}
-			}
-		}
-		visit[minNode1] = true;
-		visit[minNode2] = true;
-		answer += min;
-		min = -1;
-	}
-	return answer;
+ 
+//가중치에 따른 오름차순 정렬을 위한 비교
+bool cmp(vector<int> a, vector<int> b) {
+    return a[2] < b[2];
 }
-
-bool check(vector<bool> visit) {   // 모두 방문
-	for (int i = 0; i < visit.size(); i++) {
-		if (visit[i] == false) {
-			return true;
-		}
-	}
-	return false;
+ 
+//부모노드찾기
+int getParent(vector<int>& parent, int x) {
+    //부모노드가 자신이라면 자신을 리턴
+    if (parent[x] == x) return x;
+    //부모노드가 자신이 아니라면 최상위 부모노드 찾기
+    return parent[x] = getParent(parent, parent[x]);
+}
+ 
+//부모노드 병합하기
+void unionParent(vector<int>& parent, int a, int b) {
+    //두개의 부모를 가져와서
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    //작은 노드쪽의 부모로 병합시키기
+    a < b ? parent[b] = a : parent[a] = b;
+}
+ 
+//부모노드가 같은지 비교
+bool find(vector<int>& parent, int a, int b) {
+    //부모노드를 가져와서
+    a = getParent(parent, a);
+    b = getParent(parent, b);
+    //비교내용 리턴, 사이클 방지용
+    return a == b;
+}
+ 
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0, max = 0;
+    //가중치 기준으로 정렬
+    sort(costs.begin(), costs.end(), cmp);
+    //costs를 순회해서 제일 높은 노드찾기
+    for (auto a : costs)    if (max < a[1])
+	    max = a[1];
+    //부모노드용 리스트 생성
+    vector<int> parent;
+    //부모노드용 리스트 초기화
+    for (int i = 0; i <= max; i++)
+	    parent.push_back(i);
+    //코스트를 처음부터 순회
+    for (auto a : costs) {
+        //두개의 부모노드가 같지않다면->사이클이 생성 안된다면
+        if (!find(parent, a[0], a[1])) {
+            //결과에 가중치 추가
+            answer += a[2];
+            //부모노드 병합하기
+            unionParent(parent, a[0], a[1]);
+        }
+    }
+    return answer;
 }
