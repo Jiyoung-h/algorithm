85ddbb25586270466a45d2020ce90641f68868b0
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-17 18:07:50 +0900

    Update 가장 먼 노드.cpp

--- programmers/가장 먼 노드.cpp
+++ programmers/가장 먼 노드.cpp
@@ -1,48 +1,42 @@
 #include <string>
-#include <vector>
-#include <queue>
+#include <iostream>
 #include <algorithm>
-#include <list>
- 
+#include <queue>
+#include <vector>
+
 using namespace std;
- 
+
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    vector<vector<int>> graph(n+1, vector<int> ());
-    vector<bool> visit(n+1, false);
-    vector<int> dist(n+1, 0);
-    queue<int> q;
-    
-    for (int i = 0; i < edge.size(); i++) {
+    vector<vector<int>> graph(n+1);
+    vector<bool> check(n+1);
+    for(int i=0; i<edge.size(); i++){
         graph[edge[i][0]].push_back(edge[i][1]);
         graph[edge[i][1]].push_back(edge[i][0]);
     }
-    
-    q.push(1);
-    visit[1] = true;
-    dist[1] = 0;
-    
-    while (!q.empty()) {
-        int start = q.front();
+    queue<pair<int, int>> q;
+    q.push({1, 0});
+    check[1] = true;
+    vector<pair<int, int>> dis;
+    while(!q.empty()){
+        int pos = q.front().first;
+        int count = q.front().second;
         q.pop();
-        
-        for (int i = 0; i < graph[start].size(); i++) {
-           if (visit[graph[start][i]] == false) {
-                q.push(graph[start][i]);
-                visit[graph[start][i]] = true;
-                dist[graph[start][i]] = dist[start] + 1;
-           }
+        for(int i=0; i<graph[pos].size(); i++){
+            if(!check[graph[pos][i]]){
+                q.push({graph[pos][i], count + 1});
+                dis.push_back({count + 1, graph[pos][i]});
+                check[graph[pos][i]] = true;
+            }
         }
     }
-    
-    sort(dist.begin(), dist.end());
-    int max = dist.back();
-    
-    for (int i = 1; i <= n; i++) {
-        if (dist[i] == max) {
+    sort(dis.begin(), dis.end(), greater<pair<int, int>>());
+    int max = dis[0].first;
+    for(int i=0; i<dis.size(); i++){
+        if(max == dis[i].first)
             answer++;
-        }
+        else
+            break;
     }
-    
     return answer;
 }
