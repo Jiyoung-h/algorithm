c57627891696c92cb71311701a8ce8c32b3dc699
JiyoungHong <61260814+Jiyoung-h@users.noreply.github.com>
2021-03-18 17:20:58 +0900

    Update 이중우선순위큐.cpp

--- programmers/이중우선순위큐.cpp
+++ programmers/이중우선순위큐.cpp
@@ -1,57 +1,45 @@
 #include <string>
-#include <sstream>
 #include <vector>
 #include <queue>
-#include <map>
-using namespace std;
-vector<int> solution(vector<string> operations) {
-	vector<int> answer;
-	priority_queue<int> pq1;
-	priority_queue<int, vector<int>, greater<int>> pq2;
-	map<int, bool> check;
+#include <algorithm>
 
-	for (int i = 0; i < operations.size(); i++) {
-		string operation;
-		int number;
-		stringstream ss(operations[i]);
-		ss >> operation >> number;
-		if (operation == "I") {
-			pq1.push(number);
-			pq2.push(number);
-		}
-		else if (operation == "D") {
-			if (number == 1) {
-				while (!pq1.empty() && check[pq1.top()]) {
-					pq1.pop();
-				}
-				if (pq1.empty()) continue;
-				check[pq1.top()] = true;
-				pq1.pop();
-			}
-			else {
-				if (pq2.empty()) continue;
-				while (!pq2.empty() && check[pq2.top()]) {
-					pq2.pop();
-				}
-				if (pq1.empty()) continue;
-				check[pq2.top()] = true;
-				pq2.pop();
-			}
-		}
-	}
-	if (pq1.size() == 0 || pq2.size() == 0) {
-		answer.push_back(0);
-		answer.push_back(0);
-	}
-	else {
-		while (!pq1.empty() && check[pq1.top()]) {
-			pq1.pop();
-		}
-		answer.push_back(pq1.top());
-		while (!pq2.empty() && check[pq2.top()]) {
-			pq2.pop();
-		}
-		answer.push_back(pq2.top());
-	}
-	return answer;
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    answer.push_back(0);
+    answer.push_back(0);
+
+    vector<int> q;
+
+    for(auto o : operations) {
+        string operation = o.substr(0,1);
+        int num = stoi(o.substr(2));
+        // 삽입
+        if(operation == "I") {
+            q.push_back(num);
+            sort(q.begin(), q.end());
+        }
+        // 삭제
+        else {
+            if(q.size() == 0)
+                continue;
+            // 최대값 삭제
+            if(num == 1) {
+                q.erase(q.begin() + (q.size()-1));
+            }
+            // 최소값 삭제
+            if(num == -1) {
+                q.erase(q.begin());
+            }
+        }
+    }
+
+    if(!q.empty()) {
+        answer.clear();
+        answer.push_back(q.back());
+        answer.push_back(q.front());
+    }
+
+    return answer;
 }
